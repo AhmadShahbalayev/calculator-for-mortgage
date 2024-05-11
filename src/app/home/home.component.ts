6a462a44456c4cc7e9d276b8f637a5edda1e7b5e
Ahmad Shahbalayev <61346218+AhmadShahbalayev@users.noreply.github.com>
2024-05-12 00:13:42 +0400

    add repayment schedule

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,23 @@ import { TooltipModule } from 'primeng/tooltip';
 import { TableModule } from 'primeng/table';
 
 interface ICalculateMortgageArgs {
+  downPayment: number;
   propertyPrice: number;
   interestRate: number;
   duration: number;
+}
+
+interface IState {
+  paymentSchedule: {
+    month: number;
+    monthlyPayment: string;
+    principalPaid: string;
+    interestPaid: string;
+    remainingLoan: string;
+  }[];
+  totalPaidPrincipal: string;
+  totalPaidInterest: string;
+  totalPaidAmount: string;
 }
 
 @Component({
@@ -30,42 +44,62 @@ interface ICalculateMortgageArgs {
 })
 export class HomeComponent {
   mortgageForm = this.formBuilder.group({
+    downPayment: [null, Validators.required],
     propertyPrice: [null, Validators.required],
     interestRate: [null, Validators.required],
     duration: [null, Validators.required],
   });
 
-  state = {
-    downPayment: '-',
-    monthlyPayment: '-',
-    totalInterestPaid: '-',
-    totalAmountPaid: '-',
+  state: IState = {
+    paymentSchedule: [],
+    totalPaidPrincipal: '',
+    totalPaidInterest: '',
+    totalPaidAmount: '',
   };
 
   constructor(private formBuilder: FormBuilder) {}
 
   calculateMortgage({
+    downPayment,
     propertyPrice,
     interestRate,
     duration,
   }: ICalculateMortgageArgs) {
     const totalPayments = duration * 12;
-    const downPayment = (propertyPrice * 15) / 100;
     const loanAmount = propertyPrice - downPayment;
     const monthlyInterestRate = interestRate / (12 * 100);
-
     const monthlyPayment =
       (loanAmount * monthlyInterestRate) /
       (1 - Math.pow(1 + monthlyInterestRate, -totalPayments));
 
-    const totalPayment = monthlyPayment * totalPayments;
-    const totalInterestPaid = totalPayment - loanAmount;
+    let remainingLoan = loanAmount;
+    const paymentSchedule = [];
+    let totalPaidPrincipal = 0;
+    let totalPaidInterest = 0;
+
+    for (let i = 0; i < totalPayments; i++) {
+      const interestPayment = remainingLoan * monthlyInterestRate;
+      const principalPayment = monthlyPayment - interestPayment;
+      remainingLoan -= principalPayment;
+      totalPaidPrincipal += principalPayment;
+      totalPaidInterest += interestPayment;
+
+      paymentSchedule.push({
+        month: i + 1,
+        monthlyPayment: monthlyPayment.toFixed(),
+        principalPaid: principalPayment.toFixed(),
+        interestPaid: interestPayment.toFixed(),
+        remainingLoan: remainingLoan.toFixed(),
+      });
+    }
+
+    const totalPaidAmount = totalPaidPrincipal + totalPaidInterest;
 
     return {
-      downPayment: downPayment.toFixed(),
-      monthlyPayment: monthlyPayment.toFixed(),
-      totalInterestPaid: totalInterestPaid.toFixed(),
-      totalAmountPaid: (totalPayment + downPayment).toFixed(),
+      paymentSchedule,
+      totalPaidPrincipal: totalPaidPrincipal.toFixed(),
+      totalPaidInterest: totalPaidInterest.toFixed(),
+      totalPaidAmount: totalPaidAmount.toFixed(),
     };
   }
 
