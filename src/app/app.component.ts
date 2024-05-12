85b210630bdef20023198b5eead7cad28af5f713
Ahmad Shahbalayev <61346218+AhmadShahbalayev@users.noreply.github.com>
2024-05-12 13:03:40 +0400

    fix responsiveness and layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { PrimeNGConfig } from 'primeng/api';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: '<router-outlet />',
 })
 export class AppComponent implements OnInit {
   constructor(private primengConfig: PrimeNGConfig) {}
